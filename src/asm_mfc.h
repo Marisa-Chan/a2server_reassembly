2b1d8083d4a2c6a34b685d28a80cd4fc1daf1e3c
Marisa-Chan <thunder_8888@mail.ru>
2026-01-30 10:29:15 +0700

    Mostly CString in C++

--- src/asm_mfc.h
+++ src/asm_mfc.h
@@ -17,6 +17,18 @@ class AFX_MODULE_THREAD_STATE;
 #define AFX_DATA
 #define AFX_DATADEF
 
+
+#define ASSERT(f)          ((void)0)
+#define VERIFY(f)          ((void)(f))
+#define ASSERT_VALID(pOb)  ((void)0)
+#define DEBUG_ONLY(f)      ((void)0)
+inline void AFX_CDECL AfxTrace(LPCTSTR, ...) {}
+#define TRACE              1 ? (void)0 : ::AfxTrace
+#define TRACE0(sz)
+#define TRACE1(sz, p1)
+#define TRACE2(sz, p1, p2)
+#define TRACE3(sz, p1, p2, p3)
+
 struct CRuntimeClass;          // object type information
 
 class CObject;                        // the root of all objects classes
@@ -282,6 +294,168 @@ extern TCHAR afxChNil;
 const CString& AFXAPI AfxGetEmptyString();
 #define afxEmptyString AfxGetEmptyString()
 
+
+// CString
+_AFX_INLINE CStringData* CString::GetData() const
+{
+	ASSERT(m_pchData != NULL); return ((CStringData*)m_pchData) - 1;
+}
+_AFX_INLINE void CString::Init()
+{
+	m_pchData = afxEmptyString.m_pchData;
+}
+_AFX_INLINE CString::CString(const unsigned char* lpsz)
+{
+	Init(); *this = (LPCSTR)lpsz;
+}
+_AFX_INLINE const CString& CString::operator=(const unsigned char* lpsz)
+{
+	*this = (LPCSTR)lpsz; return *this;
+}
+#ifdef _UNICODE
+_AFX_INLINE const CString& CString::operator+=(char ch)
+{
+	*this += (TCHAR)ch; return *this;
+}
+_AFX_INLINE const CString& CString::operator=(char ch)
+{
+	*this = (TCHAR)ch; return *this;
+}
+_AFX_INLINE CString AFXAPI operator+(const CString& string, char ch)
+{
+	return string + (TCHAR)ch;
+}
+_AFX_INLINE CString AFXAPI operator+(char ch, const CString& string)
+{
+	return (TCHAR)ch + string;
+}
+#endif
+
+_AFX_INLINE int CString::GetLength() const
+{
+	return GetData()->nDataLength;
+}
+_AFX_INLINE int CString::GetAllocLength() const
+{
+	return GetData()->nAllocLength;
+}
+_AFX_INLINE BOOL CString::IsEmpty() const
+{
+	return GetData()->nDataLength == 0;
+}
+_AFX_INLINE CString::operator LPCTSTR() const
+{
+	return m_pchData;
+}
+_AFX_INLINE int PASCAL CString::SafeStrlen(LPCTSTR lpsz)
+{
+	return (lpsz == NULL) ? 0 : lstrlen(lpsz);
+}
+
+// CString support (windows specific)
+_AFX_INLINE int CString::Compare(LPCTSTR lpsz) const
+{
+	return _tcscmp(m_pchData, lpsz);
+}    // MBCS/Unicode aware
+_AFX_INLINE int CString::CompareNoCase(LPCTSTR lpsz) const
+{
+	return _tcsicmp(m_pchData, lpsz);
+}   // MBCS/Unicode aware
+// CString::Collate is often slower than Compare but is MBSC/Unicode
+//  aware as well as locale-sensitive with respect to sort order.
+_AFX_INLINE int CString::Collate(LPCTSTR lpsz) const
+{
+	return _tcscoll(m_pchData, lpsz);
+}   // locale sensitive
+
+_AFX_INLINE TCHAR CString::GetAt(int nIndex) const
+{
+	ASSERT(nIndex >= 0);
+	ASSERT(nIndex < GetData()->nDataLength);
+	return m_pchData[nIndex];
+}
+_AFX_INLINE TCHAR CString::operator[](int nIndex) const
+{
+	// same as GetAt
+	ASSERT(nIndex >= 0);
+	ASSERT(nIndex < GetData()->nDataLength);
+	return m_pchData[nIndex];
+}
+_AFX_INLINE bool AFXAPI operator==(const CString& s1, const CString& s2)
+{
+	return s1.Compare(s2) == 0;
+}
+_AFX_INLINE bool AFXAPI operator==(const CString& s1, LPCTSTR s2)
+{
+	return s1.Compare(s2) == 0;
+}
+_AFX_INLINE bool AFXAPI operator==(LPCTSTR s1, const CString& s2)
+{
+	return s2.Compare(s1) == 0;
+}
+_AFX_INLINE bool AFXAPI operator!=(const CString& s1, const CString& s2)
+{
+	return s1.Compare(s2) != 0;
+}
+_AFX_INLINE bool AFXAPI operator!=(const CString& s1, LPCTSTR s2)
+{
+	return s1.Compare(s2) != 0;
+}
+_AFX_INLINE bool AFXAPI operator!=(LPCTSTR s1, const CString& s2)
+{
+	return s2.Compare(s1) != 0;
+}
+_AFX_INLINE bool AFXAPI operator<(const CString& s1, const CString& s2)
+{
+	return s1.Compare(s2) < 0;
+}
+_AFX_INLINE bool AFXAPI operator<(const CString& s1, LPCTSTR s2)
+{
+	return s1.Compare(s2) < 0;
+}
+_AFX_INLINE bool AFXAPI operator<(LPCTSTR s1, const CString& s2)
+{
+	return s2.Compare(s1) > 0;
+}
+_AFX_INLINE bool AFXAPI operator>(const CString& s1, const CString& s2)
+{
+	return s1.Compare(s2) > 0;
+}
+_AFX_INLINE bool AFXAPI operator>(const CString& s1, LPCTSTR s2)
+{
+	return s1.Compare(s2) > 0;
+}
+_AFX_INLINE bool AFXAPI operator>(LPCTSTR s1, const CString& s2)
+{
+	return s2.Compare(s1) < 0;
+}
+_AFX_INLINE bool AFXAPI operator<=(const CString& s1, const CString& s2)
+{
+	return s1.Compare(s2) <= 0;
+}
+_AFX_INLINE bool AFXAPI operator<=(const CString& s1, LPCTSTR s2)
+{
+	return s1.Compare(s2) <= 0;
+}
+_AFX_INLINE bool AFXAPI operator<=(LPCTSTR s1, const CString& s2)
+{
+	return s2.Compare(s1) >= 0;
+}
+_AFX_INLINE bool AFXAPI operator>=(const CString& s1, const CString& s2)
+{
+	return s1.Compare(s2) >= 0;
+}
+_AFX_INLINE bool AFXAPI operator>=(const CString& s1, LPCTSTR s2)
+{
+	return s1.Compare(s2) >= 0;
+}
+_AFX_INLINE bool AFXAPI operator>=(LPCTSTR s1, const CString& s2)
+{
+	return s2.Compare(s1) <= 0;
+}
+
+
+
 /////////////////////////////////////////////////////////////////////////////
 // class CObject is the root of all compliant objects
 
@@ -394,18 +568,6 @@ public: \
 		{ pOb = (class_name*) ar.ReadObject(RUNTIME_CLASS(class_name)); \
 			return ar; } \
 			
-
-#define ASSERT(f)          ((void)0)
-#define VERIFY(f)          ((void)(f))
-#define ASSERT_VALID(pOb)  ((void)0)
-#define DEBUG_ONLY(f)      ((void)0)
-inline void AFX_CDECL AfxTrace(LPCTSTR, ...) {}
-#define TRACE              1 ? (void)0 : ::AfxTrace
-#define TRACE0(sz)
-#define TRACE1(sz, p1)
-#define TRACE2(sz, p1, p2)
-#define TRACE3(sz, p1, p2, p3)
-
 
 
 void AFXAPI AfxTryCleanup();
@@ -755,6 +917,18 @@ public:
 
 
 AFX_MODULE_THREAD_STATE* AFXAPI AfxGetModuleThreadState();
+
+
+
+
+
+
+void AFXAPI AfxThrowMemoryException();
+void AFXAPI AfxThrowNotSupportedException();
+void AFXAPI AfxThrowArchiveException(int cause,
+	LPCTSTR lpszArchiveName = NULL);
+void AFXAPI AfxThrowFileException(int cause, LONG lOsError = -1,
+	LPCTSTR lpszFileName = NULL);
 
 
 #endif
