1b345dda1521e59af292cca80eefcf360d0afb28
ivan <cool.pepyaka@gmail.com>
2026-02-03 22:16:52 +0100

    Resources and ResFile fiels

---
+++ src/resource.h
@@ -0,0 +1,39 @@
+#ifndef RES_FILE_H
+#define RES_FILE_H
+
+#include <cstdint>
+
+#include "assert_offset.h"
+#include "mfc_templ.h"
+
+
+class CFile;
+
+
+struct ResFile {
+    uint32_t magic; // 0x31415926 :D
+    uint32_t root_offset;
+    uint32_t root_size;
+    uint32_t res_flags;
+    char name[16];
+    uint32_t fat_size;
+    uint32_t size;
+    uint32_t fat_data;
+    uint32_t field_0x2c;
+    uint32_t field_0x30;
+    CFile* file;
+    uint32_t fat_offset;
+    uint16_t field_0x3c;
+    uint8_t gap_0x3e[2];
+};
+ASSERT_SIZE(ResFile, 0x40);
+
+struct Resources {
+    CArray<ResFile*> res_files;
+    CArray<void*> field_0x14;
+    CArray<CFile*> files;
+};
+ASSERT_SIZE(Resources, 0x3c);
+
+
+#endif
