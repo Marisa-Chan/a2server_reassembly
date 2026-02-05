7d0060272fc90727e40dc5b27f6cf7e8ced2e807
ivan <cool.pepyaka@gmail.com>
2026-02-05 23:08:57 +0100

    PlayersList fields

---
+++ src/players_list.cpp
@@ -0,0 +1,1 @@
+#include "players_list.h"
---
+++ src/players_list.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstdint>
+
+#include "assert_offset.h"
+#include "mfc_templ.h"
+
+
+class Player;
+
+
+class PlayersList : public CList<Player*> {
+public: // VTable at 0060f158.
+    // virtual ~PlayersList();
+    // virtual void Serialize(CArchive& ar);
+
+public:
+    uint32_t field_0x1c;
+    uint32_t next_player_id;
+};
+ASSERT_SIZE(PlayersList, 0x24);
