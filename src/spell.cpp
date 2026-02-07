58fd53411c76e75871b2f45621a4596021aa7eeb
ivan <cool.pepyaka@gmail.com>
2026-02-07 21:09:15 +0100

    Use the ASM version of SpellBook::RefreshForHumanoid

--- src/spell.cpp
+++ src/spell.cpp
@@ -1,8 +1,1 @@
 #include "spell.h"
-
-void SpellBook::RefreshForHumanoid(Humanoid* humanoid) {
-    using Fn = void (__thiscall*)(SpellBook*, Humanoid*);
-    auto sub_53DBC5 = reinterpret_cast<Fn>(0x53DBC5);
-
-    sub_53DBC5(this, humanoid);
-}
