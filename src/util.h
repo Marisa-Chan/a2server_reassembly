882dfe41e9b398e6c0fd7a98973e8d36bf4d76a4
Marisa-Chan <thunder_8888@mail.ru>
2026-02-14 22:46:25 +0700

    Game internal list's iterator template

--- src/util.h
+++ src/util.h
@@ -1,10 +1,40 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include "asm_mfc.h"
+#include "mfc_templ.h"
+
 extern "C"
 {
 	void pathStrCpy(const char* src, char* dst);
 }
+
+
+template<typename TYPE, class HLDR = CList<TYPE*> >
+struct LPIter
+{
+	HLDR *holder = nullptr;
+	POSITION position = NULL;
+
+	TYPE* Begin(HLDR &hld)
+	{
+		holder = &hld;
+		position = hld.GetHeadPosition();
+
+		if (position = NULL)
+			return nullptr;
+
+		return GetNext();
+	}
+
+	TYPE* GetNext()
+	{
+		if (position == NULL)
+			return nullptr;
+
+		return & holder->GetNext(position);
+	}
+};
 
 
 #endif
