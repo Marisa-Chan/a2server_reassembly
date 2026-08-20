3fbfbd0f6aecd094f6fb4831d2f1dbff9dc17ea5
ivan <cool.pepyaka@gmail.com>
2026-08-20 23:02:59 +0200

    Migrate `Trigger::Trigger` and `Trigger::~Trigger`

--- src/logic.cpp
+++ src/logic.cpp
@@ -12,3 +12,16 @@ TriggerCheck::TriggerCheck() {
     memset(this, 0, sizeof(TriggerCheck));
     this->exec_once = 1;
 }
+
+// 5afd79
+Trigger::Trigger() {
+    memset(this, 0, sizeof(Trigger));
+    this->checks = new CList<Check>();
+    this->actions = new CList<int32_t>();
+}
+
+// 5afe3d
+Trigger::~Trigger() {
+    delete this->checks;
+    delete this->actions;
+}
