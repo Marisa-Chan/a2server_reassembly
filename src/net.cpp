dec6b60095a98dc9262507f7c3e3b24103bee4cf
ivan <cool.pepyaka@gmail.com>
2026-08-22 16:25:07 +0200

    Migrate `NetStru1::sub_51E63F`

--- src/net.cpp
+++ src/net.cpp
@@ -4516,6 +4516,19 @@ void NetStru1::sub_51E5FB(int32_t a, int32_t b) {
     this->QueuePacketSend(&packet);
 }
 
+// 51E63F
+void NetStru1::sub_51E63F(int32_t a, int32_t b, int32_t error_code) {
+    PacketData& packet = PacketData::Inst;
+    packet.to_player_id = 0;
+    packet.id = 0xD9;
+    packet.count = 0xC;
+    uint32_t* p = (uint32_t*)packet.data;
+    *p++ = a;
+    *p++ = b;
+    *p = error_code;
+    this->QueuePacketSend(&packet);
+}
+
 // 51A6D5
 void NetStru1::sub_51A6D5(Unit* unit, Player* player, int32_t param5, int32_t param6) {
     if (unit->typeId < 0x21 || unit->typeId >= 0x40) {
