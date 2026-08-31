ff47899ffa9634688548ef7941960a4d066dad95
ivan <cool.pepyaka@gmail.com>
2026-08-31 16:02:33 +0200

    Migrate `Visibility::sub_58CE74`

--- src/visibility.cpp
+++ src/visibility.cpp
@@ -1,5 +1,6 @@
 #include "visibility.h"
 
+#include <cmath>
 #include <cstring>
 #include <memory>
 
@@ -27,6 +28,51 @@ void Visibility::sub_58CD84() {
             this->field_0x20000[20 - y][20 - x] = dist_sq;
         }
     }
+}
+
+// 58CE74
+void Visibility::sub_58CE74() {
+    for (int32_t y = 0; y <= 20; y++) {
+        for (int32_t x = 0; x <= 20; x++) {
+            uint16_t p1;
+            uint16_t p2;
+            uint16_t p3;
+            uint16_t p4;
+            if (x < (y / 2)) {
+                p1 = 0x00FF;
+                p2 = 0x0001;
+                p3 = 0x00FF;
+                p4 = 0x0001;
+            } else if (x > (y * 2)) {
+                p1 = 0xFF00;
+                p2 = 0x0100;
+                p3 = 0x0100;
+                p4 = 0xFF00;
+            } else {
+                p1 = 0xFFFF;
+                p2 = 0x0101;
+                p3 = 0x01FF;
+                p4 = 0xFF01;
+            }
+            this->field_0x22000[y + 20][x + 20] = p1;
+            this->field_0x22000[20 - y][20 - x] = p2;
+            this->field_0x22000[y + 20][20 - x] = p3;
+            this->field_0x22000[20 - y][x + 20] = p4;
+
+            int32_t max_val = (y > x) ? y : x;
+            int16_t value = 0;
+            if (max_val != 0) {
+                value = std::sqrt(y * y + x * x) * this->field_0x2a000 / max_val;
+            }
+            this->field_0x28000[y + 20][x + 20] = value;
+            this->field_0x28000[20 - y][20 - x] = value;
+            this->field_0x28000[y + 20][20 - x] = value;
+            this->field_0x28000[20 - y][x + 20] = value;
+        }
+    }
+
+    this->field_0x22000[21][20] = 0x00FF;
+    this->field_0x22000[19][20] = 0x0001;
 }
 
 // 58E1E4
