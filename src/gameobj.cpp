b007bf7ae9cd1cbf4b4f9b8bdef31bb88bb412c2
ivan <cool.pepyaka@gmail.com>
2026-09-11 22:34:17 +0200

    Migrate `CUnit::VMethod20`

--- src/gameobj.cpp
+++ src/gameobj.cpp
@@ -2786,3 +2786,16 @@ void CUnit::VMethod29()
         sample->Play(vol + g_SoundSettings.speech_pos, pan, 0, (10000 - abs(vol)) / 100, 0);
     }
 }
+
+void CUnit::VMethod20()
+{ // 469499
+    SfxSample* sample = g_SfxArray[this->action_spell + 0x1f4];
+    BigStruct2* map = this->pMapObject;
+    int32_t vol;
+    int32_t pan;
+    map->FUN_0041b7b7(this->x_pos, this->y_pos, &vol, &pan);
+
+    if (sample != nullptr) {
+        sample->Play(vol + g_SoundSettings.sfx_pos, pan, 0, (10000 - abs(vol)) / 100, 0);
+    }
+}
