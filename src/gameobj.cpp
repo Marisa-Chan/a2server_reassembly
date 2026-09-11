c1c8a3c52ed2ceda05bc3cec60881aab5237e310
ivan <cool.pepyaka@gmail.com>
2026-09-11 22:29:28 +0200

    Migrate `CUnit::VMethod24`

--- src/gameobj.cpp
+++ src/gameobj.cpp
@@ -2664,3 +2664,27 @@ void CUnit::VMethod23()
         sample->Play(vol + g_SoundSettings.speech_pos, pan, 0, (10000 - abs(vol)) / 100, 0);
     }
 }
+
+void CUnit::VMethod24()
+{ // 469a7e
+    uint32_t now = timeGetTime();
+    if (now - this->lastVoicePlaybackTick < 0xbb8) {
+        return;
+    }
+    this->lastVoicePlaybackTick = now;
+
+    SfxBank* bank = this->FUN_0046978b();
+    if (bank->swarm_count == 0) {
+        return;
+    }
+
+    SfxSample* sample = bank->swarm[Random0N(bank->swarm_count - 1)];
+    BigStruct2* map = this->pMapObject;
+    int32_t vol;
+    int32_t pan;
+    map->FUN_0041b7b7(this->x_pos, this->y_pos, &vol, &pan);
+
+    if (sample != nullptr) {
+        sample->Play(vol + g_SoundSettings.speech_pos, pan, 0, (10000 - abs(vol)) / 100, 0);
+    }
+}
