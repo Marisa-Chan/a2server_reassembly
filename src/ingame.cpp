d28ef9f737a931bb6a4400ef8f91b4acd8223126
ivan <cool.pepyaka@gmail.com>
2026-09-20 19:53:51 +0200

    Migrate `BigStruct2::sub_41A425`

--- src/ingame.cpp
+++ src/ingame.cpp
@@ -4773,6 +4773,30 @@ void BigStruct2::sub_41A33F(uint16_t x, uint16_t y)
 	}
 }
 
+// 41A425
+void BigStruct2::sub_41A425()
+{
+	POSITION it = this->field_0x9d0.GetStartPosition();
+	PacketItemOperation* pkt = &PacketItemOperation::Inst;
+	pkt->id = 0x14;
+	pkt->field_0x5 = this->my_main_unit->index;
+	pkt->to_player_id = 0;
+	pkt->count = 0;
+	while (it != nullptr) {
+		uint16_t key;
+		CGameObject* obj;
+		this->field_0x9d0.GetNextAssoc(it, key, obj);
+		if (obj->IsSelected()) {
+			pkt->AppendWord(obj->unit_id);
+		}
+	}
+	g_NetStru1_local.QueuePacketSend(pkt);
+	CUnit* unit = this->sub_41DFDB();
+	if (unit != nullptr) {
+		unit->VMethod27();
+	}
+}
+
 // 41A4DF
 void BigStruct2::sub_41A4DF()
 {
